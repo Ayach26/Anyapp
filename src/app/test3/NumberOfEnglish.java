d3c70b649361cd08e8570feb57c666f7b149b02e
Ayaka Fukuda <f26ayach@gmail.com>
2020-09-09 08:16:37 +0900

    数の英語表記プログラム

---
+++ src/app/test3/NumberOfEnglish.java
@@ -0,0 +1,12 @@
+package app.test3;
+
+/**
+ * 数の英語表記
+ *
+ */
+public class NumberOfEnglish {
+
+  public static void main(String[] args) {
+    new InputNumber().input();
+  }
+}
---
+++ src/app/test3/ap/view/OutputMessage.java
@@ -0,0 +1,17 @@
+package app.test3.ap.view;
+
+public class OutputMessage {
+
+  public void dialog() {
+    System.out.println("数字を入力してください。");
+  }
+
+  public void inputSignal() {
+    System.out.print("> ");
+  }
+  
+  public void result() {
+    System.out.print("入力された文字列はs");
+  }
+
+}
