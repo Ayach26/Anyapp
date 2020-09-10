13098d64624b92b0a338e4c727d0217a51205798
Ayaka Fukuda <f26ayach@gmail.com>
2020-09-10 23:39:52 +0900

    バブルソート処理追加

--- src.trial.java/app/test4/ap/App.java
+++ src.trial.java/app/test4/ap/App.java
@@ -7,9 +7,12 @@ import java.util.Scanner;
 
 import app.test4.ap.data.Consts;
 
+/**
+ * アルゴリズムテスト用 バブルソート(昇順)
+ */
 public class App {
   public static void main(String[] args) {
-    new App().output();
+    new App().sort();
   }
 
   private void output() {
@@ -34,5 +37,26 @@ public class App {
       e.printStackTrace();
     }
     return lines.toArray(new String[lines.size()]);
+  }
+
+  // data変数の中身が0
+  private void sort() {
+    String[] list = this.getStdin();
+//    lines.toArray(new String[lines.size()]);
+    int[] data = new int[list.length];
+    // 昇順ソート
+    for (int i = 0; i < list.length - 1; i++) {
+      for (int j = 0; j < list.length - i - 1; j++) {
+        if (data[j] > data[j + 1]) {
+          int asc = data[j];
+          data[j] = data[j + 1];
+          data[j + 1] = asc;
+        }
+      }
+    }
+    // ソート後の結果を出力
+    for (int i = 0; i < data.length; i++) {
+      System.out.println(data[i] + " ");
+    }
   }
 }
