3bce98f64d645a9f51622d1e0027f70f6b705e02
Ayaka Fukuda <f26ayach@gmail.com>
2020-09-19 08:30:43 +0900

    配列を作成後、各要素に値を代入

--- src.silver.java/app/Chapter2/Chapter2.java
+++ src.silver.java/app/Chapter2/Chapter2.java
@@ -3,5 +3,7 @@ package app.Chapter2;
 public class Chapter2 {
   public static void main(String[] args) {
     new DataVariable().variable();
+    new VarVariable().var();
+    new Array().array();
   }
 }
