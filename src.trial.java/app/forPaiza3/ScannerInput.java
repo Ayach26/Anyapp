e273061c49e82dbe136746be38206e9ef64bfe83
Ayaka Fukuda <f26ayach@gmail.com>
2020-10-06 21:36:35 +0900

    カンマ、スペース区切りの標準出力

---
+++ src.trial.java/app/forPaiza3/ScannerInput.java
@@ -0,0 +1,18 @@
+package app.forPaiza3;
+import java.util.Scanner;
+
+/**
+ * N個のデータの入力(スペース区切り)
+ */
+public class ScannerInput {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String num = sc.nextLine();
+
+    int value = Integer.parseInt(num);
+    String[] str = sc.nextLine().split(" ");
+    for (int i = 0; i < value; i++) {
+      System.out.println(str[i]);
+    }
+  }
+}
