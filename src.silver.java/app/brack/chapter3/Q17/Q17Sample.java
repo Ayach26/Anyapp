8088494f405f8fe0e79bb86048d96d52634d509e
Ayaka Fukuda <f26ayach@gmail.com>
2020-12-02 23:55:54 +0900

    黒本第3章記録用

---
+++ src.silver.java/app/brack/chapter3/Q17/Q17Sample.java
@@ -0,0 +1,14 @@
+package app.brack.chapter3.Q17;
+
+public class Q17Sample {
+  public static void main(String[] args) {
+    int num = 10;
+    if (num < 10)
+      System.out.println("A");
+    else
+      System.out.println("B");
+
+    if (num == 10)
+      System.out.println("C");
+  }
+}
