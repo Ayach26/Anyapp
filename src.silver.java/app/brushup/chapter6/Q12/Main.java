04ca1c565c3b78b6987def0a4dbcceab98a3761c
Ayaka Fukuda <f26ayach@gmail.com>
2021-01-06 23:25:56 +0900

    黒本復習

---
+++ src.silver.java/app/brushup/chapter6/Q12/Main.java
@@ -0,0 +1,9 @@
+package app.brushup.chapter6.Q12;
+
+public class Main {
+  public static void main(String[] args) {
+    Sample s = new Sample();
+    float result = s.divide(10, 2);
+    System.out.println(result);
+  }
+}
---
+++ src.silver.java/app/brushup/chapter6/Q12/Sample.java
@@ -0,0 +1,7 @@
+package app.brushup.chapter6.Q12;
+
+public class Sample {
+  float divide(int a, int b) {
+    return (float) a / (float) b;
+  }
+}
