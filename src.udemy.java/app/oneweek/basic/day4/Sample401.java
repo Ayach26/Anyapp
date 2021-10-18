497bf532eb2cc3a24e1d5a01545f0b6818ac4f04
Ayaka Fukuda <f26ayach@gmail.com>
2021-10-18 17:51:40 +0900

    基本編4日目ループ処理(for文)

---
+++ src.udemy.java/app/oneweek/basic/day4/Sample401.java
@@ -0,0 +1,9 @@
+package app.oneweek.basic.day4;
+
+public class Sample401 {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+      System.out.println(i);
+    }
+  }
+}
