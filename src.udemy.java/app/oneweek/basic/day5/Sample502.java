2804d839ed4788a5cc510a4f0455d5889051ab11
Ayaka Fukuda <f26ayach@gmail.com>
2021-10-20 10:05:58 +0900

    基本編5日目配列変数(ジャグ配列)

--- src.udemy.java/app/oneweek/basic/day5/Sample502.java
+++ src.udemy.java/app/oneweek/basic/day5/Sample502.java
@@ -2,15 +2,12 @@ package app.oneweek.basic.day5;
 
 public class Sample502 {
   public static void main(String[] args) {
-    int n[] = { 5, 4, 3, 2, 1 };
-    String s[] = { "ABC", "DFG", "GHI" };
-    int i;
-
-    for (i = 0; i < n.length; i++) {
-      System.out.println(n[i]);
-    }
-    for (i = 0; i < s.length; i++) {
-      System.out.println(s[i]);
+    int a[][] = { { 0, 1 }, { 2 }, { 3, 4, 5, 6 } };
+    for (int m = 0; m < a.length; m++) {
+      for (int n = 0; n < a[m].length; n++) {
+        System.out.print(a[m][n] + " ");
+      }
+      System.out.println();
     }
   }
 }
