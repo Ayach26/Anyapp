4208b4525e1396f40f5087e5a366e70ffb2b464f
Ayaka Fukuda <f26ayach@gmail.com>
2021-10-20 15:21:37 +0900

    基本編7日目アクセス修飾子とカプセル化

---
+++ src.udemy.java/app/oneweek/basic/day7/Sample701.java
@@ -0,0 +1,10 @@
+package app.oneweek.basic.day7;
+
+public class Sample701 {
+
+  public static void main(String[] args) {
+    SampleClass02 s = new SampleClass02();
+    s.method2();
+    s.method3();
+  }
+}
---
+++ src.udemy.java/app/oneweek/basic/day7/SampleClass03.java
@@ -0,0 +1,27 @@
+package app.oneweek.basic.day7;
+
+public class SampleClass03 {
+
+  private int number = 0;
+  private String str = "";
+
+  public SampleClass03(String str) {
+    this.str = str;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public String getStr() {
+    return str;
+  }
+
+//  public void setStr(String str) {
+//    this.str = str;
+//  }
+}
