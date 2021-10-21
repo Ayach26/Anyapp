3f2dfa1fce4fb79f9ca47ad0cc5eec44f4822dde
Ayaka Fukuda <f26ayach@gmail.com>
2021-10-21 10:49:54 +0900

    応用編1日目静的メンバ(自分自身のメソッド)

---
+++ src.udemy.java/app/oneweek/basic/day8/SampleEx102.java
@@ -0,0 +1,29 @@
+package app.oneweek.basic.day8;
+
+public class SampleEx102 {
+
+  public static void main(String[] args) {
+    SampleEx102 i = new SampleEx102();
+    i.foo();
+    // インスタンスからも静的メソッドを呼べるが推奨しない(インスタンスメソッドの誤解が生まれる)
+    i.bar();
+    //　クラス名からの呼び出し
+    SampleEx102.bar();
+    // クラス名なしの呼び出し
+    bar();
+    // ガーベージコレクタの呼び出し
+    System.gc();
+  }
+
+  public SampleEx102() {
+    System.out.println("コンストラクタ");
+  }
+
+  public void foo() {
+    System.out.println("インスタンスメソッド");
+  }
+
+  public static void bar() {
+    System.out.println("静的メソッド");
+  }
+}
