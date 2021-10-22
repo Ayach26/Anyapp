7152020da05314da4f813f1cc1897840867f0f6f
Ayaka Fukuda <f26ayach@gmail.com>
2021-10-22 16:19:28 +0900

    応用編4日目インターフェースと抽象クラス

---
+++ src.udemy.java/app/oneweek/basic/exday4/ICannon.java
@@ -0,0 +1,7 @@
+package app.oneweek.basic.exday4;
+
+public interface ICannon {
+  public String WEAPON_NAME = "cannon";
+
+  public void fire();
+}
---
+++ src.udemy.java/app/oneweek/basic/exday4/ICar.java
@@ -0,0 +1,7 @@
+package app.oneweek.basic.exday4;
+
+public interface ICar {
+  public String TYPE_NAME = "car";
+
+  public void move();
+}
